f13cda41c43537fa982d3b1cbca5766dea7044ec
Grier Phillips <GrierPhillips@gmail.com>
2017-10-13 15:57:19 -0600

    Fix erroneous comment

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ import shutil
 
 from openpyxl import load_workbook
 import pandas as pd
-# import win32com.client as win32
+import win32com.client as win32
 from xlrd import open_workbook
 from xlutils.copy import copy
 
