731c8be9fc66224aae4be5ddb68e11b93daa28f0
Grier Phillips <GrierPhillips@gmail.com>
2017-10-13 13:59:35 -0600

    Add argparsing for iterations, input, and output paths

--- update_cdap.py
+++ update_cdap.py
@@ -1,9 +1,30 @@
 #! /usr/bin/env/python
 
+import argparse
 
-def update_cdap(arg):
+
+PARSER = argparse.ArgumentParser(
+    description='Update the coordinated daily activity pattern calibration '
+    'workbooks and uec.')
+PARSER.add_argument(
+    'iteration', metavar='Iteration', type=int, default=0,
+    help='The current calibration iteration number.'
+)
+PARSER.add_argument(
+    '-i', '--input_path', metavar='Input_Path', type=str,
+    default='../../newpopsyn',
+    help='The path to the directory containing the abm output and uec ' +
+    'directories.')
+PARSER.add_argument(
+    '-o', '--output_path', metavar='Output_Path', type=str, default='.',
+    help='The path to the directory containing the model calibration files.')
+ARGS = PARSER.parse_args()
+
+
+def update_cdap(iter_, input_path, output_path):
     pass
 
 
 if __name__ == '__main__':
-    update_cdap()
+    update_cdap(
+        ARGS.iteration, ARGS.input_path, ARGS.output_path)
