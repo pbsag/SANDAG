59a7a9dbea50050f19911fc2ca58929be67211d1
Grier Phillips <GrierPhillips@gmail.com>
2017-10-13 15:51:55 -0600

    Start update.py
    This file will be the home for a function that will wrap different update functions.

---
+++ update.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env/python
+
+import argparse
+from os.path import abspath
+import shutil
+
+from openpyxl import load_workbook
+import pandas as pd
+import win32com.client as win32
+from xlrd import open_workbook
+from xlutils.copy import copy
+
+
+def update(method=None):
+    pass
+
+if __name__ == '__main__':
+    update()
