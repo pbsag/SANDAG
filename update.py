4a1b09dff346bb7feaee18dc304b3e82269e73b1
Grier Phillips <GrierPhillips@gmail.com>
2017-10-13 15:56:51 -0600

    Implement argpars
    Implemented argparse to parse the iteration, type of update, and paths for input and output.

--- update.py
+++ update.py
@@ -6,9 +6,31 @@ import shutil
 
 from openpyxl import load_workbook
 import pandas as pd
-import win32com.client as win32
+# import win32com.client as win32
 from xlrd import open_workbook
 from xlutils.copy import copy
+
+
+PARSER = argparse.ArgumentParser(
+    description='Update the coordinated daily activity pattern calibration '
+    'workbooks and uec.')
+PARSER.add_argument(
+    'iteration', metavar='Iteration', type=int, default=0,
+    help='The current calibration iteration number.'
+)
+PARSER.add_argument(
+    'type', metavar='Type', type=str, default='AO', choices=['AO', 'CDAP'],
+    help='The type of update to perform.'
+)
+PARSER.add_argument(
+    '-i', '--input_path', metavar='Input_Path', type=str,
+    default='../../newpopsyn',
+    help='The path to the directory containing the abm output and uec ' +
+    'directories.')
+PARSER.add_argument(
+    '-o', '--output_path', metavar='Output_Path', type=str, default='.',
+    help='The path to the directory containing the model calibration files.')
+ARGS = PARSER.parse_args()
 
 
 def update(method=None):
