531ba578358bc587571fbdde94fac136b77f17f9
Grier Phillips <GrierPhillips@gmail.com>
2017-10-18 16:38:15 -0600

    Add calibrate function
    The calibrate function os the top level controller for the automation process.
    This function takes all of the parameters used in launching transCAD, setting
    up and launching the abm and updating the calibration files. Depending on the
    step the function will wait until a specific file is updated, kill transCAD
    and all child processes and update calibration files. This will greatly increase
    the speed of calibration activities and remove the human element (potential error).

--- calibrate.py
+++ calibrate.py
@@ -8,11 +8,19 @@ in sequence.
 
 """
 
+import os.path as osp
 import subprocess
-from time import sleep
+from time import sleep, time
 
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
+
+from update import update
+
+
+FILES = {'AO': ['AutoOwnership', 'aoResults.csv', '1_AO'],
+         'CDAP': ['CoordinatedDailyActivityPattern', 'personData_{}.csv',
+                  '2_CDAP']}
 
 
 def launch_transcad():
@@ -118,3 +126,46 @@ def launch_abm(working_directory):
     board.tap_key(board.backspace_key)
     board.tap_key(board.tab_key)
     board.tap_key(board.enter_key)
+
+
+def calibrate(working_directory, start_iter=1, sample_rate=None, max_iters=3,
+              input_path='.', output_path='../Model Calibration'):
+    """Calibrate abm with given parameters.
+
+    Parameters
+    ----------
+    working_directory : str
+        The path to the directory containing the gisdk and uec directories.
+    start_iter : int, default : 1
+        The iteration to start the process from.
+    sample_rate : float or str, default : None
+        The sample rate for the given starting iteration, or a string
+        representing all of the sample rates.
+    max_iters : int, default : 3
+        The maximum number of iterations to run for each step.
+    iput_path : str, default : '.'
+        The relative path to the output and uec directories.
+    output_path : str, default : '../Model Calibration'
+        The relative path to the directory containing the calibration
+        directories.
+
+    """
+    steps = ['AO', 'CDAP', 'DC', 'MC', 'ML', 'TR', 'AS']
+    for step in steps:
+        cal_path = output_path + '/{}'.format(FILES[step][2])
+        update(0, input_path, cal_path, method=step)
+        for iter_ in range(max_iters):
+            proc = launch_transcad()
+            setup_abm(working_directory, start_iter=start_iter,
+                      sample_rate=sample_rate)
+            launch_abm(working_directory)
+            start_time = time()
+            sleep(18000)
+            last_mod = osp.getmtime(FILES[step][1].format(start_iter))
+            while last_mod < start_time:
+                sleep(1800)
+                last_mod = osp.getmtime(FILES[step][1].format(start_iter))
+            kill_proc_tree(proc.pid, including_parent=True)
+            update(iter_ + 1, input_path, cal_path, method=step)
+            print('Completed Step {} iteration {}.\n'
+                  .format(FILES[step][0], iter_ + 1))
