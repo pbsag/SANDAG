c9860391e999af5187a311a6f59514c1128a9bd8
Grier Phillips <GrierPhillips@gmail.com>
2017-10-28 00:42:45 -0600

    Remove PARSER as update cannot be imported otherwise on SANDAG pc

--- update.py
+++ update.py
@@ -11,26 +11,26 @@ from xlrd import open_workbook
 from xlutils.copy import copy
 
 
-PARSER = argparse.ArgumentParser(
-    description='Update the coordinated daily activity pattern calibration '
-    'workbooks and uec.')
-PARSER.add_argument(
-    'iteration', metavar='Iteration', type=int, default=0,
-    help='The current calibration iteration number.'
-)
-PARSER.add_argument(
-    'type', metavar='Type', type=str, default='AO', choices=['AO', 'CDAP'],
-    help='The type of update to perform.'
-)
-PARSER.add_argument(
-    '-i', '--input_path', metavar='Input_Path', type=str,
-    default='../../newpopsyn',
-    help='The path to the directory containing the abm output and uec ' +
-    'directories.')
-PARSER.add_argument(
-    '-o', '--output_path', metavar='Output_Path', type=str, default='.',
-    help='The path to the directory containing the model calibration files.')
-ARGS = PARSER.parse_args()
+# PARSER = argparse.ArgumentParser(
+#     description='Update the coordinated daily activity pattern calibration '
+#     'workbooks and uec.')
+# PARSER.add_argument(
+#     'iteration', metavar='Iteration', type=int, default=0,
+#     help='The current calibration iteration number.'
+# )
+# PARSER.add_argument(
+#     'type', metavar='Type', type=str, default='AO', choices=['AO', 'CDAP'],
+#     help='The type of update to perform.'
+# )
+# PARSER.add_argument(
+#     '-i', '--input_path', metavar='Input_Path', type=str,
+#     default='../../newpopsyn',
+#     help='The path to the directory containing the abm output and uec ' +
+#     'directories.')
+# PARSER.add_argument(
+#     '-o', '--output_path', metavar='Output_Path', type=str, default='.',
+#     help='The path to the directory containing the model calibration files.')
+# ARGS = PARSER.parse_args()
 
 
 def replace_values(dest, data):
