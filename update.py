fc787c61a631d9721bd02b8c981bf20b44d898c3
Grier Phillips <GrierPhillips@gmail.com>
2017-11-02 15:32:12 -0600

    Set update.py up as module rather than command line script
    The previous plan was to use the update.py script from the command line, but with the new
    automated framework it is no longer necessary. As a result the PARSER is no longer
    necessary.;

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env/python
+"""This module contians all functions related to updating calibration files."""
 
-import argparse
+
 from os.path import abspath
 import shutil
 
@@ -9,28 +9,6 @@ import pandas as pd
 import win32com.client as win32
 from xlrd import open_workbook
 from xlutils.copy import copy
-
-
-# PARSER = argparse.ArgumentParser(
-#     description='Update the coordinated daily activity pattern calibration '
-#     'workbooks and uec.')
-# PARSER.add_argument(
-#     'iteration', metavar='Iteration', type=int, default=0,
-#     help='The current calibration iteration number.'
-# )
-# PARSER.add_argument(
-#     'type', metavar='Type', type=str, default='AO', choices=['AO', 'CDAP'],
-#     help='The type of update to perform.'
-# )
-# PARSER.add_argument(
-#     '-i', '--input_path', metavar='Input_Path', type=str,
-#     default='../../newpopsyn',
-#     help='The path to the directory containing the abm output and uec ' +
-#     'directories.')
-# PARSER.add_argument(
-#     '-o', '--output_path', metavar='Output_Path', type=str, default='.',
-#     help='The path to the directory containing the model calibration files.')
-# ARGS = PARSER.parse_args()
 
 
 def replace_values(dest, data):
@@ -270,7 +248,3 @@ def update_cdap(iter_, wb_name, results, uec_path, cal_path):
 
     update_uec(uec_path, 88, 6, new_m_const)
     update_uec(uec_path, 88, 7, new_n_const)
-
-
-if __name__ == '__main__':
-    update()
